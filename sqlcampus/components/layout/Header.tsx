aae2036e136ab1b8f2b20e421698911c3d532a56
Mathis Neunzig <mathis.neunzig@gmail.com>
2023-11-05 12:58:41 +0100

    dev page

--- sqlcampus/components/layout/Header.tsx
+++ sqlcampus/components/layout/Header.tsx
@@ -14,11 +14,6 @@ const Header = ({ title }: Props) => (
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <header>
-        <nav>
-            <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-            <Link href="/users">Users List</Link> |{' '}
-            <a href="/api/users">Users API</a>
-        </nav>
         </header>
     </>
 );
--- sqlcampus/pages/index.tsx
+++ sqlcampus/pages/index.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/link'
 import Layout from '../components/layout/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
+    <h1>Hi there, welcome to sql-campus.de! 🎉</h1>
+    <br></br>
+    <br></br>
+    <p>This site is currently in development. Please check again later 🫶🏼</p>
+    <p>For further questions, please contact <a href='mailto:info@sql-campus.de'>info@sql-campus.de ✉️</a></p>
   </Layout>
 )
 
